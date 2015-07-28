ec966a13b011401e5915c719eab56a7660a0983e
amcvey <andmcvey@gmail.com>
2015-07-28 18:06:58 -0400

    Deleting subsidiary controller and config n form

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,9 @@ class Course < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
 
   has_many :subsidiaries, dependent: :destroy
+  accepts_nested_attributes_for :subsidiaries,
+    reject_if: proc { |attr| attr['address'].blank? }
+    allow_destroy: true
   validates :name, presence: true
-  
+
 end
