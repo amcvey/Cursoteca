c6b42dd337cd91be59a6bb1a88aad343bb700ac6
amcvey <andmcvey@gmail.com>
2015-07-28 11:15:36 -0400

    adding cancancan gem

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
   # GET /courses
