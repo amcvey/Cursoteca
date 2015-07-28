7d0efcdcad3fafa6c34658ceb1700aac36c12047
amcvey <andmcvey@gmail.com>
2015-07-28 19:21:53 -0400

    Adding company references to course

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728184733) do
+ActiveRecord::Schema.define(version: 20150728232003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,19 @@ ActiveRecord::Schema.define(version: 20150728184733) do
     t.string   "photo"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "company_id"
   end
+
+  add_index "courses", ["company_id"], name: "index_courses_on_company_id", using: :btree
+
+  create_table "subsidiaries", force: :cascade do |t|
+    t.string   "address"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "subsidiaries", ["course_id"], name: "index_subsidiaries_on_course_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -56,4 +68,6 @@ ActiveRecord::Schema.define(version: 20150728184733) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "courses", "companies"
+  add_foreign_key "subsidiaries", "courses"
 end
