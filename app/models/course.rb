c935a9fbbe41fdc65a061e069b81d7ab2956b04b
amcvey <andmcvey@gmail.com>
2015-07-28 20:58:44 -0400

    Configuring version_courses model with views

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,9 @@ class Course < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
   belongs_to :company
   has_many :subsidiaries
+  has_many :version_courses, dependent: :destroy
+  accepts_nested_attributes_for :version_courses,
+    reject_if: proc {|attr| attr['price', 'commission', 'session_number', 'start_date', 'end_date', 'room'].blank? }
 
   def to_s
     name
