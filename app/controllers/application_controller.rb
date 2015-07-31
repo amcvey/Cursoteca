c88849b768fc89cda6f8d1ec8a3c8394ec7d02de
amcvey <andmcvey@gmail.com>
2015-07-31 18:57:03 -0400

    adding phone to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
       redirect_to root_url, :alert => exception.message
     end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :phone
+      devise_parameter_sanitizer.for(:account_update) << :phone
+    end
+
     
   
   # Prevent CSRF attacks by raising an exception.
